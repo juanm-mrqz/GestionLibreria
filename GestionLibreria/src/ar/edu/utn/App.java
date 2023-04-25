ae55be16285ef7f21bd06327687ed4f5e8759cdd
juanm-mrqz <106544182+juanm-mrqz@users.noreply.github.com>
2023-04-25 17:54:07 -0300

    Add files via upload

--- GestionLibreria/src/ar/edu/utn/App.java
+++ GestionLibreria/src/ar/edu/utn/App.java
@@ -5,21 +5,26 @@ public class App {
         
         //Crear Productos
         Magazine revista = new Magazine("Titulo de una revista ", "2020-05-12", "Pepe", 201201, 200.40); 
-
         Product libro = new Book("IT", "Stephen", "King", "2001-02-12", "Editorial", 290113, 230.90);
         
         //Crear item del carrito
         Item i1 = new Item(libro, 2, true); 
         Item i2 = new Item(revista, 4, false); 
         
+        Item i3 = new Item (new Book(), 2, false); 
+        i3.setProducto(new Book("titulo","name", "lastname", "1998-02-01", "Lalo", 2010111, 120.40));
+
         //Crear carrito
         Chart carrito = new Chart();
+
         //Agregando items al carrito
         carrito.addItem(i1);
         carrito.addItem(i2);
+        carrito.addItem(i3);
 
         //Mostrando resultados
         carrito.showItems();
+
 
     }
 }
--- GestionLibreria/src/ar/edu/utn/Chart.java
+++ GestionLibreria/src/ar/edu/utn/Chart.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 
 public class Chart {
     //Collection para agregar productos
-    ArrayList<Item> productos = new ArrayList<Item>();
+    private ArrayList<Item> productos;
 
     //Constructor por default
-    public Chart() {}; 
+    public Chart() {
+        this.productos = new ArrayList<Item>();
+    }; 
 
-    //Plantear Constructor con parametros? 
+
 
 
     //METODOS
     //Agregar producto al carrito
     public void addItem(Item p){
         this.productos.add(p); 
+    }
+
+    //Devolver Carrito
+
+    public ArrayList<Item> getChart(){
+        return this.productos; 
     }
 
     //Mostrar los items del carrito
@@ -28,6 +36,18 @@ public class Chart {
         }
         System.out.println("\n"); //salto de linea
         System.out.println("Total: " + getTotalPrice());
+    }
+
+    //Eliminar item del carrito
+    public void removeItem(Item item){
+        if (getChart().contains(item)){
+            this.productos.remove(item);
+        }
+    }
+
+    //Eliminar todo el carrito
+    public void removeChart(){
+        this.productos = new ArrayList<Item>();
     }
 
     //Precio total del carrito
